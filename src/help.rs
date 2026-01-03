606e399a6ca96fd5c30aaee08d7517faa623f243
D R <metricstream@gmail.com>
2026-01-02 23:44:17 -0800

    making help clearer

--- src/help.rs
+++ src/help.rs
@@ -10,22 +10,21 @@ Options:
   -logfile <file>        use given file for log
   -l       <file>          ''
   -noinput               do not allow interactive input
-  -buildfile <file>      use given build file
+  -buildfile <file>      use the given script file
   -file    <file>        ''
   -f       <file>        ''
   -keep-going, -k        execute all targets that do not depend
                          on failed target(s)
-  -dry-run               do not launch any executable, but show their parameters
+  -dry-run               do not launch any executable, but show their arguments
   -r                     execute all targets accordingly dependencies anyway
   -D<property>=<value>   use a value for a given property name
   -propertyfile <name>   load all properties from file with -D
                          properties taking precedence
-  -input                 input responses read from a pipe instead of a console
-  -find [<file>]         (s)earch for build file towards the root of 
-  -s    [<file>]         the filesystem and use it 
-  -targethelp            print all target names in a build file with descriptions/comments
+  -find [<file>]         (s)earch for a script file towards the root of 
+  -s    [<file>]         the filesystem and then use it 
+  -targethelp            print all target names in the script file with descriptions/comments
   -th                    ''
-  --                     a separator of argumets passed to the build target executable
+  --                     a separator of argumets passed to the script target executable
 Examples: rb jar -d
           rb compile -s
           rb clean compile -r
