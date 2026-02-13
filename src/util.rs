54490c196eea70349fa211d9ea6cc8af2f4a9768
D R <metricstream@gmail.com>
2026-02-12 19:35:28 -0800

    little reading proof

--- src/util.rs
+++ src/util.rs
@@ -298,7 +298,7 @@ use std::time::UNIX_EPOCH;
 #[inline]
 pub fn year_now() -> u64 {
     SystemTime::now()
-    .duration_since(UNIX_EPOCH).expect("Time went backwards")
+    .duration_since(UNIX_EPOCH).unwrap_or_default()
     .as_secs() / 31556952 + 1970
 }
 
